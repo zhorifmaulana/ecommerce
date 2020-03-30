86aa1b1dd0c354719aa5050ab283bcf7f8bd3487
zhorifmaulana <zhorif.maulana@ui.ac.id>
2020-03-31 00:45:14 +0700

    shop data added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import './App.css';
 import {Switch, Route} from 'react-router-dom';
 import HomePage from './Pages/Homepage/homepage.component';
+import ShopPage from './Components/Shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App()  {
     return (
     <div>
       <Switch>
         <Route exact path ='/' component = {HomePage} />
-        <Route exact path ='/hats' component = {HatsPage} />
+        <Route exact path ='/shop' component = {ShopPage} />
       </Switch>
     </div>
   );
