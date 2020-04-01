1b441c1fd274e7cc7991cc49c67abaa6a892ee15
zhorifmaulana <zhorif.maulana@ui.ac.id>
2020-04-01 14:28:18 +0700

    adding button,form input component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Switch, Route} from 'react-router-dom';
 import HomePage from './Pages/Homepage/homepage.component';
 import ShopPage from './Components/Shop/shop.component';
 import Header from './Components/header/header.component';
+import SignInAndSignUpPage from './Pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App()  {
     return (
@@ -12,6 +13,7 @@ function App()  {
       <Switch>
         <Route exact path ='/' component = {HomePage} />
         <Route exact path ='/shop' component = {ShopPage} />
+        <Route exact path ='/signin' component = {SignInAndSignUpPage} />
       </Switch>
     </div>
   );
