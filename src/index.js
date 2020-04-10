fb2bdc8200c781acbdb16c3461aec6a822ebe3e5
zhorifmaulana <zhorif.maulana@ui.ac.id>
2020-04-10 22:08:24 +0700

    redux setup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux';
+import store from './redux/store';
+
+
 
 ReactDOM.render(
+  <Provider store = {store}>
   <BrowserRouter>
   <App />
   </BrowserRouter>
+  </Provider>
   , document.getElementById('root'));
 
 serviceWorker.register();
